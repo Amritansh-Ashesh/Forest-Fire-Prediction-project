b556cc783b21b60da3cdba4e36916420ee502482
Bhawika16 <62383299+Bhawika16@users.noreply.github.com>
2020-09-15 14:11:58 +0530

    Add files via upload

--- flaskfinal.py
+++ flaskfinal.py
@@ -1,16 +1,15 @@
-from flask import Flask,request, url_for, redirect, render_template
-import pickle
-import numpy as np
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 model=pickle.load(open('model.pkl','rb'))
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+@app.route('/')
 
 @app.route('/')
 def hello_world():
     return render_template("forest_fire.html")
-
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
@@ -27,5 +26,21 @@ def predict():
         return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
 
 
-if __name__ == '__main__':
+@app.route('/home')
+def home():
+    return render_template('home.html',title='homepage',status='')
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='about',status='')
+
+@app.route('/predictor')
+def predictor():
+    return render_template('predictor.html',title='Predictor',status='')
+
+@app.route('/services')
+def services():
+    return render_template('services.html',title='services',status='')
+
+if __name__ =='__main__':
     app.run(debug=True)
