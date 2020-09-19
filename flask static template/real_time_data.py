7e6ba93d2e7561c703e9a9bfe11acf3ddcc36a29
Tedddddddyyyyy <Tedddddddyyyyy@github.com>
2020-09-19 19:59:23 +0530

    Updates
    Added Donation Page
    Added Prediction Page
    Changed Styling

--- flask static template/real_time_data.py
+++ flask static template/real_time_data.py
@@ -10,11 +10,11 @@ def fetch_data(loc="Kaziranga National Park"):
     res = requests.get(f'https://api.darksky.net/forecast/1c9424849fc849dc22a73f5c96032111/{location.latitude},{location.longitude}')
     json_data = json.loads(res.text)
 
-    temp = json_data['currently']['temperature']
-    humidity = json_data['currently']['humidity']
-    wind_vel = json_data['currently']['windSpeed']
+    # temp = json_data['currently']['temperature']
+    # humidity = json_data['currently']['humidity']
+    # wind_vel = json_data['currently']['windSpeed']
 
-    return [temp, humidity, wind_vel]
+    return json_data['currently'].items()
 
 # ---------------Print All Real-Time Data--------------#
 # for key,value in json_data['currently'].items():
