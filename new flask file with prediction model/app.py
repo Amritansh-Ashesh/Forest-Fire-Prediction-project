c20f8d774e5ea11d63861099ec1083441ad53acc
Tedddddddyyyyy <Tedddddddyyyyy@github.com>
2020-10-26 20:45:19 +0530

    Updated Frontend

--- new flask file with prediction model/app.py
+++ new flask file with prediction model/app.py
@@ -3,29 +3,14 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 model=pickle.load(open('model.pkl','rb'))
 
+
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html',title='homepage',status='')
+def hello_world():
+    return render_template("forest_fire.html")
 
-@app.route('/about')
-def about():
-    return render_template('about.html',title='about',status='')
-
-@app.route('/predictor')
-def predictor():
-    return render_template('predictor.html',title='Predictor',status='')
-
-@app.route('/forest_fire')
-def forest_fire():
-    return render_template('forest_fire.html', title='homepage', status='')
-
-@app.route('/services')
-def services():
-    return render_template('services.html',title='services',status='')
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
--- new flask file with prediction model/flaskfinal.py
+++ new flask file with prediction model/flaskfinal.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for
-import pickle
 
 app = Flask(__name__)
 
@@ -7,6 +6,26 @@ model=pickle.load(open('model.pkl','rb'))
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
+
+@app.route('/')
+def hello_world():
+    return render_template("forest_fire.html")
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction=model.predict_proba(final)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+
+    if output>str(0.5):
+        return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
+    else:
+        return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
+
+
 @app.route('/home')
 def home():
     return render_template('home.html',title='homepage',status='')
@@ -22,20 +41,6 @@ def predictor():
 @app.route('/services')
 def services():
     return render_template('services.html',title='services',status='')
-
-@app.route('/predict',methods=['POST','GET'])
-def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=model.predict_proba(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
-
-    if output>str(0.5):
-        return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
-    else:
-        return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
 
 if __name__ =='__main__':
     app.run(debug=True)
